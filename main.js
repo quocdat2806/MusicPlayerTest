411f0d9214a66875bc1d27cffdbbcd79f21fcd8e
tranquocdat <dath33603@gmail.com>
2023-05-02 10:37:06 +0700

    fix

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ const player = document.querySelector('.player')
 const nextBtn = document.querySelector('.btn-next')
 const backBtn = document.querySelector('.btn-prev')
 const repeatBtn = document.querySelector('.btn-repeat')
+const iconRepeat = document.querySelector('.icon-repeat')
 
+const randomBtn = document.querySelector('.btn-random')
 const app = {
     songs  : [
         {
@@ -54,6 +56,7 @@ const app = {
     ],
     currentIndex:0,
     isRepeat:false,
+    isRandom : false,
     loadSong:function(){
       
         const vitridautien = app.songs[app.currentIndex] ;
@@ -105,18 +108,20 @@ const app = {
             app.isPlaying = true 
             audio.play();
           }
-            
-
-           
+          
         }
         //khi audio được phát thì chạy vào đây
 
-        const progress = document.querySelector('input[type="range"]') // đây là 1 cách khác để lấy ra thẻ input thôi
+        const progress = document.querySelector('input[type="range"]') 
+        // đây là 1 cách khác để lấy ra thẻ input thôi
         // có thể lấy theo class cũng được
         // hàm này sẽ chạy khi bài hát được phát
         audio.ontimeupdate =function () {
+        
             // lấy ra tổng số giây của bài hát
             const duration = audio.duration
+
+           
             if(audio.currentTime){
                 //bình thường mới bắt đầu bài hát thì audio.currentTime === NAN
                 // nên ta phải kiểm tra rằng không có NAN thì mới chạy đoạn này
@@ -139,41 +144,49 @@ const app = {
                 //chúng ta nhận được biến progressPercent là 1 số nguyên
                 //rồi cúng ta gán vào thẻ input có thuộc tính là value
                 const progressPercent  = Math.floor( audio.currentTime / duration * 100 );
-                progress.value = progressPercent
+                progress.value = progressPercent 
             }
 
         }
         // khi chúng ta tua
+
+        
         progress.onchange = function(e) {
+            console.log(progress.value) // 18%
            // lấy giá trị ở chỗ tua
            const progressValue = e.target.value;//lấy được phần trăm của bài hát khi tua
            const duration = audio.duration // lấy số giây của bài hát
 
            //từ phần trăm bài hát lấy ra được số giây
            const currenDuration = progressValue /100 * duration // chỗ này tư duy xíu là đc
+           console.log(currenDuration)
            // ví dụ bài hát có 400 giây 
            // mà ta có 15% 
            //thì 15% của 400s sẽ là 15/100* 400
            //kết quả sẽ là số giây đó
            // bây giờ gán ngược số giây này vào audio
            //những thuộc tính này a tra gg sẽ rõ
-          audio.currentTime = currenDuration
+          audio.currentTime = currenDuration 
           e.target.value = progressValue
-
-
-
-
-
-
 
         }
         // khi next 
+        
         nextBtn.onclick = function () {
+            if(app.isRandom) {
+                let random = Math.floor(Math.random()*app.songs.length)
+                app.currentIndex = random
+            }
             app.currentIndex ++ ; // lấy ra vị trí của bài hát hiện tại cộng thêm 1 
+            console.log(app.currentIndex)
             if(app.currentIndex >= app.songs.length ){
                 //nếu mà vị trí của bài hát đến  cuối của mảng thì chúng ta phải 
                 // cho bằng 0 để phát lại từ đầu
                 app.currentIndex = 0 
+            }
+            if(app.isPlaying){
+                // nếu mà bài hát trươc đang được hát thì khi chuyển bài cũng cho hát luôn
+                audio.play()
             }
             // đặt lại tên bài hát ca sĩ cái src của thẻ audio
             // như ở chỗ load songs
@@ -182,15 +195,23 @@ const app = {
             const linkAnh = app.songs [app.currentIndex].image
             anh.style.backgroundImage = `url('${linkAnh}')`
             audio.src = `${app.songs[app.currentIndex].path}`
-            if(app.isPlaying){
-                // nếu mà bài hát trươc đang được hát thì khi chuyển bài cũng cho hát luôn
-                audio.play()
-            }
+           
 
 
 
         }
         backBtn.onclick = function () {
+
+            if(app.isRandom) {
+                let random = Math.floor(Math.random()*app.songs.length)
+                if(random === app.currentIndex) {
+                    random = Math.floor(Math.random()*app.songs.length)
+                    
+                }
+                app.currentIndex = random
+    
+                
+            }
             app.currentIndex -- ; // lấy ra vị trí của bài hát hiện tại trừ thêm 1 
             if(app.currentIndex <0 ){
                 //nếu mà ở vị trí đầu tiên thì 
@@ -210,9 +231,10 @@ const app = {
             }
 
         }
+       
         repeatBtn.onclick = function(e) {
             // thêm class active cho nó để nó biến thành màu đỏ
-            app.isRepeat = !app.isRepeat
+            app.isRepeat = ! app.isRepeat
             // câu lệnh này có ngĩa là đặt cái isRepeat bằng cái phủ định của giá trị của nó
             // ví dụ nó đang bằng false thì sẽ thành true
             // và true thì nó thành false
@@ -224,30 +246,42 @@ const app = {
             this.classList.toggle('active',app.isRepeat) // khi true thì thêm class active 
             //khi false thì gỡ bỏ class active
         }
+
+        randomBtn.onclick = function () {
+            app.isRandom = ! app.isRandom
+            this.classList.toggle('active',app.isRandom) // khi true thì thêm class active 
+
+
+        }
         //khi hết bài hát
         audio.onended = function (){
-            // có thể app.currentIndex ++
-            //nhưng làm luôn thế này kết quả cũng tương tự 
-            //hàm này có nghĩa chúng ta tự click vào nút next
-            // và khi chúng ta click vào nút next thì hàm nextBtn.onclick ở trên lại được chạy
+           
             // nhưng trước tiên kiểm tra nó có được nhấn vào nút phát lại không 
             if(app.isRepeat){
                 //nêu được nhấn thì cho phát lại
                 audio.play()
 
             }else{
+            // có thể app.currentIndex ++
+            //nhưng làm luôn thế này kết quả cũng tương tự 
+            //hàm này có nghĩa chúng ta tự click vào nút next
+            // và khi chúng ta click vào nút next thì hàm nextBtn.onclick ở trên lại được chạy
 
-                nextBtn.click()
+                  nextBtn.click()
+
+
+
             }
-
         }
+
+
         //còn nút random logic cũng như vậy nhá a
         // gợi ý 
         // a dùng Math.random() nhân với độ dài của mảng và dùng math.floor() để làm tròn dưới
         // gợi ý Math.floor(Math.random()*app.songs.length)
         // chỗ này anh gán bằng biến app.currentIndex =Math.floor(Math.random()*app.songs.length)
         // rồi a gán nội dung bài hát ca sĩ ảnh và src của audio bằng mảng ở vị trí currentIndex là được
-        
+
 
     
     
